43b67133cd30e5511a205d1e3d859c3f6be52f82
otukutun <orehaorz@gmail.com>
2015-02-16 21:52:04 +0900

    add top page

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
+
+  root 'pages#top'
+
   get 'users/my'
 
   get 'users/index'
 
   devise_for :users, controllers:  { omniauth_callbacks: 'users/omniauth_callbacks' }
 
-  devise_scope :user do
-    get 'sign_in', to: 'devise/sessions#new', as: :new_user_session
-    get 'sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session
-  end
+  #devise_scope :user do
+  #  get 'sign_in', to: 'devise/sessions#new', as: :new_user_session
+  #  get 'sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session
+  #end
 
   # devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
