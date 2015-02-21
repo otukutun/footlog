3882017c8b6defcee03530940298ed33c2a3c9f5
otukutun <orehaorz@gmail.com>
2015-02-21 14:50:45 +0900

    can add name to uses tables.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
@@ -6,4 +8,10 @@ class ApplicationController < ActionController::Base
   #  def new_session_path(scope)
   #    new_user_session_path
   #  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :name
+  end
 end
